a379139f5d811a63f18b6ac07015f7ade01f7044
lipeiru <peiru.li@advance.ai>
2020-11-21 23:11:41 +0800

    lesson 2

---
+++ 2-terminal-hacker/Assets/WM2000/play.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class play : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        beforeStart();
+    }
+
+    void beforeStart() {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Hello World!");
+        Terminal.WriteLine("Hello World!");
+        Terminal.WriteLine("Hello World!");
+        Terminal.WriteLine("Hello World!");
+    }
+
+    void OnUserInput(string input) {
+        Terminal.WriteLine("The input: " + input);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
